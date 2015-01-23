30c0dd0eafe3d5e3a112e2873dd937ae0c18b848
NonGT <thenongt@gmail.com>
2015-01-23 16:12:55 +0700

    Add templated_view method to MY_Loader

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -3,4 +3,12 @@
 /* load the MX_Loader class */
 require APPPATH."third_party/MX/Loader.php";
 
-class MY_Loader extends MX_Loader {}
+class MY_Loader extends MX_Loader {
+	
+	function templated_view($template_view, $content_view, $data = null, $as_string = FALSE, $content_var = "content")
+	{
+	    $content = $this->load->view($content_view, $data, true);
+	    $data[$content_var] = &$content;
+	    $this->load->view($template_view, $data, $as_string);
+	}
+}
