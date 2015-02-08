3add5322f26a8fcb28df55e4c3b531cd446354be
NonGT <thenongt@gmail.com>
2015-02-08 21:17:43 +0700

    Force using server environment var to store AWS keys

--- application/modules/setting/controllers/setting_admin.php
+++ application/modules/setting/controllers/setting_admin.php
@@ -10,6 +10,25 @@ class Setting_admin extends MX_Controller {
 
 	function general()
 	{
-		$this->load->view("setting");
+		if($this->input->server("AWS-ACCESS-KEY-ID") && $this->input->server("AWS-SECRET-ACCESS-KEY"))
+		{
+			$access_key = $this->input->server("AWS-ACCESS-KEY-ID");
+			$access_key = "********************".substr($access_key, count($access_key) - 5);
+			
+			$secret = $this->input->server("AWS-SECRET-ACCESS-KEY");
+			$secret = "********************".substr($secret, count($secret) - 5);
+		}
+		else
+		{
+			$access_key = null;
+			$secret = null;
+		}
+		
+		$this->data = array(
+			"aws_access_key_id"	=> $access_key,
+			"aws_secret_access_key" => $secret
+		);
+		
+		$this->load->view("setting", $this->data);
 	}
 }
--- application/modules/setting/views/setting.php
+++ application/modules/setting/views/setting.php
@@ -1,5 +1,5 @@
-<section class="n-content">
-	<form class="uk-form">
+<div class="n-content">
+	<form class="uk-form" novalidate="">
 
 		<div class="uk-panel uk-panel-box">
 			<div class="uk-panel-title">
@@ -34,7 +34,8 @@
 			</div>
 			<div class="uk-grid">
 				<div class="uk-width-1-1 uk-width-medium-1-3 uk-width-large-1-4">
-					<input type="radio" name="uploadOption" id="uploadOptionDisable" value="disable" checked="">
+					<input type="radio" name="uploadOption" id="uploadOptionDisable" 
+						   ng-model="uploadOption" value="disable" checked="" />
 					<label for="uploadOptionDisable"><?= lang("setting_file_upload_disabled") ?></label>
 				</div>
 				<div class="uk-hidden-small uk-width-1-1 uk-width-medium-2-3 uk-width-large-3-4 uk-text-muted">
@@ -44,17 +45,25 @@
 			<hr/>
 			<div class="uk-margin-small-top uk-grid">
 				<div class="uk-width-1-1 uk-width-medium-1-3 uk-width-large-1-4">
-					<input type="radio" name="uploadOption" id="uploadOptionFileSystem" value="file">
+					<input type="radio" name="uploadOption" id="uploadOptionFileSystem" value="file" 
+						   ng-model="uploadOption" />
 					<label for="uploadOptionFileSystem"><?= lang("setting_file_upload_file_system") ?></label>
 				</div>
 				<div class="uk-hidden-small uk-width-1-1 uk-width-medium-2-3 uk-width-large-3-4 uk-text-muted">
 					<?= lang("setting_file_upload_file_system_desc") ?>
 				</div>
 			</div>
+			<div class="uk-margin-top uk-panel uk-panel-box n-setting-details ng-hide"
+				 ng-show="uploadOption == 'file'">
+				<label class="uk-form-label" for="uploadFilePath">Upload Root Path</label>
+				<input type="text" class="uk-width-1-1 uk-margin-small-top" id="uploadFilePath" 
+					   placeholder="Enter path elative to your web root directory" />
+			</div>
 			<hr/>
 			<div class="uk-margin-small-top uk-grid">
 				<div class="uk-width-1-1 uk-width-medium-1-3 uk-width-large-1-4">
-					<input type="radio" name="uploadOption" id="uploadOptionDatabase" value="db">
+					<input type="radio" name="uploadOption" id="uploadOptionDatabase" value="db" 
+						   ng-model="uploadOption" />
 					<label for="uploadOptionDatabase"><?= lang("setting_file_upload_database") ?></label>
 				</div>
 				<div class="uk-hidden-small uk-width-1-1 uk-width-medium-2-3 uk-width-large-3-4 uk-text-muted">
@@ -64,12 +73,92 @@
 			<hr/>
 			<div class="uk-margin-small-top uk-grid">
 				<div class="uk-width-1-1 uk-width-medium-1-3 uk-width-large-1-4">
-					<input type="radio" name="uploadOption" id="uploadOptionS3" value="s3">
+					<input type="radio" name="uploadOption" id="uploadOptionS3" value="s3" 
+						   ng-model="uploadOption" />
 					<label for="uploadOptionS3"><?= lang("setting_file_upload_s3") ?></label>
 				</div>
 				<div class="uk-hidden-small uk-width-1-1 uk-width-medium-2-3 uk-width-large-3-4 uk-text-muted">
 					<?= lang("setting_file_upload_s3_desc") ?>
 				</div>
+			</div>
+			<div class="uk-margin-top uk-panel uk-panel-box n-setting-details ng-hide"
+				 ng-show="uploadOption == 's3'">
+				<?php if(!($aws_access_key_id && $aws_secret_access_key)) : ?>
+				<div class="uk-alert uk-alert-warning">
+					For security reason, AWS Access Key ID and AWS Secret Access Key must be setup
+					by using server environment variables as AWS-ACCESS-KEY-ID and AWS-SECRET-ACCESS-KEY.
+					Please follow the instruction <a href="#aws-key-instruction" data-uk-modal="">here</a>.
+				</div>
+				<?php else : ?>
+				<div class="uk-alert uk-alert-success">
+					You have successfully setup AWS Access Key ID and AWS Secret Access Key. 
+					If you want to change it please update server environment variables by 
+					following the instruction <a href="#aws-key-instruction" data-uk-modal="">here</a>.
+				</div>
+				<?php endif ?>
+				<div>
+					<label class="uk-form-label" for="uploadS3Key">Access Key</label>
+					<input type="text" class="uk-width-1-1 uk-margin-small-top" id="uploadS3Key" 
+						   placeholder="AWS Access Key ID" maxlength="200"
+						   value="<?= $aws_access_key_id ?>" readonly />
+				</div>
+				<div class="uk-margin-small-top">
+					<label class="uk-form-label" for="uploadS3Secret">Secret Key</label>
+					<input type="text" class="uk-width-1-1 uk-margin-small-top" id="uploadS3Secret" 
+						   placeholder="AWS Secret Access Key" maxlength="200"
+						   value="<?= $aws_secret_access_key ?>" readonly />
+				</div>
+				<div class="uk-margin-small-top">
+					<label class="uk-form-label" for="uploadS3Bucket">Bucket</label>
+					<input type="text" class="uk-width-1-1 uk-margin-small-top" id="uploadS3Bucket" 
+						   placeholder="Bucket Name (will be created if not exists)" 
+						   maxlength="30" />
+				</div>
+			</div>
+			
+			<div id="aws-key-instruction" class="uk-modal">
+			    <div class="uk-modal-dialog">
+			        <a class="uk-modal-close uk-close"></a>
+			        <h2>Setup environment variables for AWS</h2>
+			        <p>
+				        Due to the security concerns, Zimpler will just force you to always use environment variable 
+				        to store any secret data instead of database or configuration file.
+			        </p>
+			        <hr/>
+			        <p>
+				        In Apache, you can put the following lines into httpd.conf
+						<div class="n-code-block">
+							<code>SetEnv AWS_ACCESS_KEY_ID &lt;AWS-ACCESS-KEY-ID&gt;</code><br/>
+							<code>SetEnv AWS_SECRET_ACCESS_KEY &lt;AWS-SCRET-ACCESS-KEY&gt;</code>
+						</div>
+			        </p>
+			        <p>
+						Just replace <code>&lt;AWS-ACCESS-KEY-ID&gt;</code> and <code>&lt;AWS-SCRET-ACCESS-KEY&gt;</code>
+						with the actual keys you own and restart the web server.
+			        </p>
+			        <p>
+				        <span class="uk-badge">Note</span> If you are using <b>alias</b>, the above code
+				        must be put under its <code>&lt;Directory&gt;</code> block.
+			        </p>
+			        <hr/>
+			        <p>
+				        If you host it on Enginx, the above variables must be set as fastcgi_param as following:
+						<div class="n-code-block">
+							<code>fastcgi_param AWS_ACCESS_KEY_ID &lt;AWS-ACCESS-KEY-ID&gt;</code><br/>
+							<code>fastcgi_param AWS_SECRET_ACCESS_KEY &lt;AWS-SCRET-ACCESS-KEY&gt;</code>
+						</div>
+			        </p>
+			        <p>
+				        Please see this
+				        <a href="http://nginx.org/en/docs/http/ngx_http_fastcgi_module.html" target="_blank">link</a> 
+				        for more details.
+			        </p>
+			        <hr/>
+			        <p>
+				        In Heroku, you can use its UI to reveals all Config Variables. Then put
+				        <code>AWS_ACCESS_KEY_ID</code> and <code>AWS_SECRET_ACCESS_KEY</code> as keys.
+			        </p>
+			    </div>
 			</div>
 		</div>
 		
@@ -146,4 +235,4 @@
 		</div>
 
 	</form>
-</section>
+</div>
