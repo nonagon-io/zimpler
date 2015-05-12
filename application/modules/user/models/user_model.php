6d1b57f6e9ba2a9c112a2c2f33489db5ae364d9c
NonGT <thenongt@gmail.com>
2015-05-13 05:53:07 +0700

    Use identity var  to check existing username instead

--- application/modules/user/models/user_model.php
+++ application/modules/user/models/user_model.php
@@ -61,9 +61,9 @@ class User_model extends CI_Model {
 	{
 		$tables = $this->config->item('tables', 'ion_auth');
 
-		$this->db->where('username', $username);
+		$this->db->where($this->config-item('identity'), $username);
 		$this->db->from($tables["users"]);
-		
+
 		return $this->db->count_all_results() > 0;
 	}
 }
