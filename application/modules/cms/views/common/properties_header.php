0acade52e2a2dd3de05a62e514eef43bdbc86725
NonGT <thenongt@gmail.com>
2015-07-28 15:02:58 +0700

    Fix properties header displaying when facing long title

--- application/modules/cms/views/common/properties_header.php
+++ application/modules/cms/views/common/properties_header.php
@@ -1,10 +1,10 @@
-<div class="uk-panel-title uk-grid">
-	<div class="uk-width-1-2">
-		<div style="margin-top: 5px">
+<div class="uk-panel-title uk-grid uk-grid-collapse">
+	<div class="uk-width-2-3">
+		<div class="n-header-title">
 			<i class="uk-icon-edit"></i> {{editingData.headerTitle}}
 		</div>
 	</div>
-	<div class="uk-width-1-2 uk-text-right" style="font-size: 14px">
+	<div class="uk-width-1-3 uk-text-right" style="font-size: 14px">
 		<div class="uk-display-inline-block" style="margin-top: 3px; margin-right: 5px;">
 			Rev. {{editingData.revision}}
 		</div>
